0a8dab5c06c208aec9c1b13b765e01f963e1d8cd
Jeremy Carbaugh <jcarbaugh@gmail.com>
2010-09-16 10:30:11 -0400

    update to django 1.2.3 and add 1.2.3 admin media url

--- settings.py
+++ settings.py
@@ -46,7 +46,7 @@ MEDIA_URL = '/media/'
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/admin/media/'
+ADMIN_MEDIA_PREFIX = 'http://assets.sunlightfoundation.com/admin/1.2.3/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'ulup-)leyhvxf*-k*sjpp8=l(od^7noayu1-3l78s&fqiwp718'
